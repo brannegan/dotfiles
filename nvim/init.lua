61b94cba744f8ecea80716a779863db115c7026c
zapp <zapp_branegan@gmail.com>
2022-04-26 09:58:08 +0700

    Neovim. LSP setting moved to plugins/rust-tools

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,29 +1,24 @@
--- Map leader to space
-
+-- Auto install packer.nvim if not exists
 local fn = vim.fn
 local execute = vim.api.nvim_command
 local autocmd = vim.api.nvim_create_autocmd
-
--- Sensible defaults
-require('options')
-
--- Auto install packer.nvim if not exists
 local install_path = fn.stdpath('data') .. '/site/pack/packer/opt/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
     execute('!git clone https://github.com/wbthomason/packer.nvim ' ..
                 install_path)
 end
 vim.cmd [[packadd packer.nvim]]
---autocmd("BufWritePost", {pattern = "use.lua", callback = function() require'packer'.compile() end})
+-- Recompile packer on write
+autocmd("BufWritePost", {pattern = "use.lua", callback = function() require'packer'.compile() end})
+
+-- Sensible defaults
+require('options')
 
 -- Install plugins
 require('use')
 
 -- Key mappings
 require('keymaps')
-
--- LSP
-require('lsp')
 
 -- Colorscheme
 require('colorscheme')
--- nvim/lua/use.lua
+++ nvim/lua/use.lua
@@ -5,72 +5,74 @@ end
 return require('packer').startup(function()
 
     -- Packer can manage itself as an optional plugin
-    use {'wbthomason/packer.nvim', opt = true}
+   use {'wbthomason/packer.nvim', opt = true}
 
-    -- Development
-    --use {'tpope/vim-fugitive'}
-    use {'tpope/vim-surround'}
-    use {'tpope/vim-repeat'}
-    use {'windwp/nvim-autopairs',
-      config = get_config("autopairs")
-    }
-
-    -- Color scheme
-    use {'kyazdani42/nvim-web-devicons',
-      config = get_config("devicon")
-    }
-    use {'sainnhe/gruvbox-material'}
-
-    -- Telescope
-    use {'nvim-lua/plenary.nvim'}
-    use {'nvim-lua/popup.nvim'}
-    use {'nvim-telescope/telescope.nvim',
-      requires = {
-        'nvim-telescope/telescope-ui-select.nvim',
-      },
-      config = get_config("telescope")
-    }
-
-    -- LSP config
-    use {'neovim/nvim-lspconfig'}
-    use {'nvim-lua/lsp-status.nvim'}
-
-    -- Completion - use either one of this
-    use {'hrsh7th/nvim-cmp', 
-      requires = {
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'saadparwaiz1/cmp_luasnip',
-      },
-      config = get_config("nvim-cmp")
-    }
-    -- Snippets
-    use { 'L3MON4D3/LuaSnip' }
-
-    -- Better syntax
-    use {'nvim-treesitter/nvim-treesitter',
-          run = ':TSUpdate',
-          config = get_config("treesitter")
-    }
-    --use {'nvim-treesitter/nvim-treesitter-textobjects'}
-    --use {'nvim-treesitter/playground'}
-
-    use {'hoob3rt/lualine.nvim', 
-      config = get_config("lualine")
-    }
+   -- Development
+   --use {'tpope/vim-fugitive'}
+   use {'tpope/vim-surround'}
+   use {'tpope/vim-repeat'}
+   use {'windwp/nvim-autopairs',
+     config = get_config("autopairs")
+   }
   
-    -- DAP
-    -- use {'mfussenegger/nvim-dap'}
-    -- use {'nvim-telescope/telescope-dap.nvim'}
-    -- use {'theHamsta/nvim-dap-virtual-text'}
+   -- Color scheme
+   use {'sainnhe/gruvbox-material'}
 
-    -- Rust
-    use {'simrat39/rust-tools.nvim'}
+   -- Telescope
+   use {'nvim-telescope/telescope.nvim',
+     requires = {
+       'nvim-lua/popup.nvim',
+       'nvim-lua/plenary.nvim',
+       'kyazdani42/nvim-web-devicons',
+       'nvim-telescope/telescope-ui-select.nvim',
+     },
+     config = get_config("telescope")
+   }
 
-    -- Terminal
-    use {"akinsho/toggleterm.nvim",
-      config = get_config("toggleterm")
-    }
+   -- Completion - use either one of this
+   use {'hrsh7th/nvim-cmp', 
+     requires = {
+       'hrsh7th/cmp-nvim-lsp',
+       'hrsh7th/cmp-buffer',
+       'hrsh7th/cmp-path',
+       'saadparwaiz1/cmp_luasnip',
+     },
+     config = get_config("nvim-cmp")
+   }
+
+   -- LSP for Rust
+   use {'simrat39/rust-tools.nvim',
+     ft = 'rust',
+     config = get_config("rust-tools"),
+     requires = { 
+       'neovim/nvim-lspconfig',
+       'nvim-lua/lsp-status.nvim',
+       'hrsh7th/cmp-nvim-lsp',
+     },
+   }
+  
+   -- Snippets
+   use { 'L3MON4D3/LuaSnip' }
+
+   -- Better syntax
+   use {'nvim-treesitter/nvim-treesitter',
+         run = ':TSUpdate',
+         config = get_config("treesitter")
+   }
+   --use {'nvim-treesitter/nvim-treesitter-textobjects'}
+   --use {'nvim-treesitter/playground'}
+   use {'hoob3rt/lualine.nvim', 
+     config = get_config("lualine")
+   }
+
+   -- DAP
+   -- use {'mfussenegger/nvim-dap'}
+   -- use {'nvim-telescope/telescope-dap.nvim'}
+   -- use {'theHamsta/nvim-dap-virtual-text'}
+  
+   -- Terminal
+   use {"akinsho/toggleterm.nvim",
+     config = get_config("toggleterm")
+   }
 
 end)
