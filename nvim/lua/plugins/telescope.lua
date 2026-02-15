e320d136bc7a250701d52e614e3c6c9c7b79aff6
Brannegan <zapp.brannegan@gmail.com>
2026-02-15 20:28:49 +0700

    changed neovim telescope layout to ivy

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,36 +6,28 @@ return { 'nvim-telescope/telescope.nvim',
         'nvim-telescope/telescope-ui-select.nvim',
     },
     keys = {
-        {'<leader>ff', "<cmd>Telescope find_files<CR>", desc = "Telescope"},
-        {'<leader>fg', "<cmd>Telescope git_files<CR>", desc = "Telescope"},
-        {'<leader>rg', "<cmd>Telescope live_grep<CR>", desc = "Telescope"},
-        {'<leader>b',  "<cmd>Telescope buffers<CR>", desc = "Telescope"},
-        {'<leader>re', "<cmd>Telescope lsp_references<CR>", desc = "Telescope"},
-        {'<leader>i',  "<cmd>Telescope lsp_implementations<CR>", desc = "Telescope"},
-        {'<leader>sy', "<cmd>Telescope lsp_document_symbols<CR>", desc = "Telescope"},
-        {'<leader>d',  "<cmd>Telescope diagnostics<CR>", desc = "Telescope"},
+        {'<leader>ff', "<cmd>Telescope find_files<CR>", desc = "Telescope find files"},
+        {'<leader>fg', "<cmd>Telescope git_files<CR>", desc = "Telescope git files"},
+        {'<leader>rg', "<cmd>Telescope live_grep<CR>", desc = "Telescope live grep"},
+        {'<leader>b',  "<cmd>Telescope buffers<CR>", desc = "Telescope buffers"},
+        {'<leader>re', "<cmd>Telescope lsp_references<CR>", desc = "Telescope lsp references"},
+        {'<leader>im', "<cmd>Telescope lsp_implementations<CR>", desc = "Telescope lsp impls"},
+        {'<leader>sy', "<cmd>Telescope lsp_document_symbols<CR>", desc = "Telescope lsp doc symbols"},
+        {'<leader>d',  "<cmd>Telescope diagnostics<CR>", desc = "Telescope diagnostics"},
     },
     config = function()
-        local actions = require 'telescope.actions'
-        local previewers = require 'telescope.previewers'
-
         require('telescope').setup {
             defaults = {
-                mappings = {
-                    i = {
-                        ["<c-j>"] = actions.move_selection_next,
-                        ["<c-k>"] = actions.move_selection_previous,
-                    },
-                },
                 file_ignore_patterns = {"target/*"},
-                file_previewer = previewers.vim_buffer_cat.new,
-                grep_previewer = previewers.vim_buffer_vimgrep.new,
-                qflist_previewer = previewers.vim_buffer_qflist.new
             },
             pickers = {
-                find_files = {
-                    no_ignore = true,
-                }
+                find_files           = { theme = "ivy", layout_config = { height = 0.4 } },
+                live_grep            = { theme = "ivy", layout_config = { height = 0.4 } },
+                git_files            = { theme = "ivy", layout_config = { height = 0.4 } },
+                lsp_references       = { theme = "ivy", layout_config = { height = 0.4 } },
+                lsp_implementations  = { theme = "ivy", layout_config = { height = 0.4 } },
+                lsp_document_symbols = { theme = "ivy", layout_config = { height = 0.4 } },
+                diagnostics          = { theme = "ivy", layout_config = { height = 0.4 } }
             },
             extensions = {
                 ["ui-select"] = {
